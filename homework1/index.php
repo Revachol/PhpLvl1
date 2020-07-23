3e11622c666d94520823e4fe45e6a0192d83f2f5
Degor1470 <egorov.dima1470@gmail.com>
2020-07-23 23:08:43 +0300

    homework1

---
+++ homework1/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!-- Задание 2 -->
+    <?php
+    $title = "minimalistica";
+    $h1 = "Minimalistica";
+    $year = 3;
+    ?>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <meta name="author" content="Luka Cvrk (www.solucija.com)" />
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+<!-- Задание 1 -->
+<?php
+$a = 5;
+$b = '05';
+var_dump($a == $b);         // Почему true?    //php имеет слабую типизацию, из-за этого он преобразует перменую а в int и убирает 0 (так как в числе первый ноль не имеет смысла)
+var_dump((int)'012345');     // Почему 12345? //команда (int) преобразует последущую запись в число значит 012345 становиться числом и 0 убирается так как не имеет смысла
+var_dump((float)123.0 === (int)123.0); // Почему false? // === это сравнение по типу данных, в данном случае они имеют разные типы float (дробное число) и int(целое число)
+var_dump((int)0 === (int)'hello, world'); // Почему true? //=== сравнивает типы данных, в данном случае int и int, поэтому true
+?>
+
+<!-------------------------------------------------------------------------------------------------------------------------------------------------->
+<hr>
+
+
+<div id="content">
+    <h1><?php echo $h1; ?></h1>
+
+    <ul id="menu">
+        <li><a href="#">home</a></li>
+        <li><a href="#">archive</a></li>
+        <li><a href="#">contact</a></li>
+    </ul>
+
+    <div class="post">
+        <div class="details">
+            <h2><a href="#">Nunc commodo euismod massa quis vestibulum</a></h2>
+            <p class="info">posted <?php echo $year; ?> hours ago in <a href="#">general</a></p>
+
+        </div>
+        <div class="body">
+            <p>Nunc eget nunc libero. Nunc commodo euismod massa quis vestibulum. Proin mi nibh, dignissim a pellentesque at, ultricies sit amet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem eu libero laoreet facilisis. Aenean placerat, ligula quis placerat iaculis, mi magna luctus nibh, adipiscing pretium erat neque vitae augue. Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at ipsum.</p>
+        </div>
+        <div class="x"></div>
+    </div>
+
+    <div class="col">
+        <h3><a href="#">Ut enim risus rhoncus</a></h3>
+        <p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+        <p>&not; <a href="#">read more</a></p>
+    </div>
+    <div class="col">
+        <h3><a href="#">Maecenas iaculis leo</a></h3>
+        <p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+        <p>&not; <a href="#">read more</a></p>
+    </div>
+    <div class="col last">
+        <h3><a href="#">Quisque consectetur odio</a></h3>
+        <p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+        <p>&not; <a href="#">read more</a></p>
+    </div>
+
+    <div id="footer">
+        <?= date('Y')?>
+        <p>Copyright &copy; <em>minimalistica</em> &middot; Design: Luka Cvrk, <a href="http://www.solucija.com/" title="Free CSS Templates">Solucija</a></p>
+    </div>
+</div>
+<?php
+$a = 1;
+$b = 2;
+$b = $a + $b;
+$a = $b - $a;
+$b = $b - $a;
+echo $a;
+echo $b;
+?>
+</body>
+</html>
